61266d2bd40e3dc5346ee27906f93bf6b20425b1
ewgenius <ewgeniux@gmail.com>
2016-10-29 23:18:39 +0300

    init twitter api

--- build/app.js
+++ build/app.js
@@ -1,11 +1,49 @@
 "use strict";
+var dotenv_1 = require('dotenv');
 var request = require('request');
-var consumer = 'BN5GEnTp0eLwjdqwJCctdS198';
-var secret = 'HBIZLpUJ2Ifq72hLHDBRZk5bvvZFWOFFAChOAYN3PvInldHerC';
-var url = 'https://api.twitter.com/1.1/trends/place.json';
-request(url, {
-    method: 'POST'
-}, function (error, response, body) {
-    console.log(response.statusCode);
-    console.log(error, body);
+dotenv_1.config();
+var API = 'https://api.twitter.com';
+var TWITTER_CONSUMER = process.env.TWITTER_CONSUMER;
+var TWITTER_SECRET = process.env.TWITTER_SECRET;
+function authorize(consumerKey, secretKey) {
+    var code = new Buffer(encodeURIComponent(consumerKey) + ':' + encodeURIComponent(secretKey)).toString('base64');
+    return new Promise(function (resolve, reject) {
+        request(API + "/oauth2/token", {
+            method: 'post',
+            headers: {
+                'Authorization': "Basic " + code,
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            },
+            json: true,
+            body: 'grant_type=client_credentials'
+        }, function (error, response, body) {
+            if (error)
+                reject(error);
+            else
+                resolve(body.access_token);
+        });
+    });
+}
+function requestTrends(token) {
+    return new Promise(function (resolve, reject) {
+        request("https://api.twitter.com/1.1/trends/place.json?id=1", {
+            json: true,
+            headers: {
+                'Authorization': "Bearer " + token,
+            }
+        }, function (error, response, body) {
+            if (error)
+                reject(error);
+            resolve(body);
+        });
+    });
+}
+authorize(TWITTER_CONSUMER, TWITTER_SECRET)
+    .then(function (accessToken) {
+    return requestTrends(accessToken);
+})
+    .then(function (response) {
+    response[0].trends.map(function (trend) {
+        console.log(trend.name, trend.tweet_volume);
+    });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,67 @@
+import { config as configDotenv } from 'dotenv'
 import * as http from 'http'
 import * as request from 'request'
 
+configDotenv()
+const API = 'https://api.twitter.com'
 
-const url = 'https://api.twitter.com/1.1/trends/place.json'
+const TWITTER_CONSUMER = process.env.TWITTER_CONSUMER
+const TWITTER_SECRET = process.env.TWITTER_SECRET
 
-request(url, {
-  method: 'POST'
-}, (error: any, response: http.IncomingMessage, body: any) => {
-  console.log(response.statusCode)
-  console.log(error, body)
-})
+function authorize(consumerKey: string, secretKey: string): Promise<string> {
+  const code = new Buffer(encodeURIComponent(consumerKey) + ':' + encodeURIComponent(secretKey)).toString('base64')
+
+  return new Promise<string>((resolve, reject) => {
+    request(`${API}/oauth2/token`, {
+      method: 'post',
+      headers: {
+        'Authorization': `Basic ${code}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+      json: true,
+      body: 'grant_type=client_credentials'
+    }, (error: any, response: http.IncomingMessage, body: any) => {
+      if (error)
+        reject(error)
+      else resolve(body.access_token)
+    })
+  })
+}
+
+interface Trend {
+  name: string
+  url: string
+  promoted_content: any
+  query: string
+  tweet_volume: number
+}
+
+interface TrendResponse {
+  trends: Trend[]
+}
+
+type TrendsResponse = TrendResponse[]
+
+function requestTrends(token: string): Promise<TrendsResponse> {
+  return new Promise<TrendsResponse>((resolve, reject) => {
+    request(`https://api.twitter.com/1.1/trends/place.json?id=1`, {
+      json: true,
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      }
+    }, (error: any, response: http.IncomingMessage, body: any) => {
+      if (error) reject(error)
+      resolve(body)
+    })
+  })
+}
+
+authorize(TWITTER_CONSUMER, TWITTER_SECRET)
+  .then(accessToken => {
+    return requestTrends(accessToken)
+  })
+  .then(response => {
+    response[0].trends.map(trend => {
+      console.log(trend.name, trend.tweet_volume)
+    })
+  })
